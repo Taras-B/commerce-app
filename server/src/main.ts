3c3d3775474bfce1ca663aa4dd403f9913a90987
Taras-B <taras5475@gmail.com>
2021-02-27 16:01:58 +0200

    Add typescript in react app and header component

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-// jOkoP5YH0Mls6Kj3
-//mongodb+srv://tarasb:<password>@cluster0.d51o4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   const options = new DocumentBuilder()
     .setTitle('commerce')
-    .setDescription('The IvorySoft messenger API')
+    .setDescription('The Commerce API')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
