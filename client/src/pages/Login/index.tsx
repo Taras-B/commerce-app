f45ef989a1fec6c53234f6525a0ee0691e41e409
Taras-B <taras5475@gmail.com>
2021-03-02 16:05:18 +0200

    Update validation in login schema

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -11,6 +11,7 @@ const loginSchema = yup.object({
   password: yup
     .string()
     .min(8, 'Password should be of minimum 8 characters length')
+    .max(20, 'Password should be of maximum 20 characters length')
     .required('Password is required'),
 })
 
