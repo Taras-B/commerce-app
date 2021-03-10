fb8791cb86af95c54c72c4f4ca85949ce8d8cbc1
Taras-B <taras5475@gmail.com>
2021-03-10 14:23:09 +0200

    Update Home page,Add category

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
+
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+
 export const Home = () => {
-  return <div>home page</div>
+  return (
+    <Grid container justify='center' spacing={3}>
+      <Grid item md={3} xs={12}>
+        <Box color='text.primary' bgcolor='text.secondary' borderRadius={16}>
+          <Grid container spacing={2}>
+            <Grid item xs>
+              <Typography align='center'>Category</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant='outlined'>Phone</Button>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button variant='outlined'>Fridge</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+      <Grid item md={9} xs={12}>
+        Items
+      </Grid>
+    </Grid>
+  )
 }
